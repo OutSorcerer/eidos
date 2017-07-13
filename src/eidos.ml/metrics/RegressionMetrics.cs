3ef061f66b2a26ffacace30bcf8b652657cba7ba
Evgeniy Mamchenko <evgeniy.mamchenko@yandex.ru>
2017-07-14 01:21:52 +0200

    Implemented MSE and an extension method for regression estimators.

---
+++ src/eidos.ml/metrics/RegressionMetrics.cs
@@ -0,0 +1,13 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.Statistics;
+
+namespace eidos.ml.metrics
+{
+    public static class RegressionMetrics
+    {
+        public static double MeanSquaredError(Vector<double> yTrue, Vector<double> yPred)
+        {
+            return (yTrue - yPred).PointwisePower(2).Mean();
+        }
+    }
+}
---
+++ src/eidos.ml/metrics/RegressionMetricsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using eidos.ml.abstractions;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.Statistics;
+
+namespace eidos.ml.metrics
+{
+    public static class RegressionMetricsExtensions
+    {
+        public static double MeanSquaredError<TR, TE>(this IRegressionEstimator<TR, TE> estimator, Matrix<double> x, Vector<double> y)
+            where TR : IRegression<TR, TE>
+            where TE : IRegressionEstimator<TR, TE>
+        {
+            return RegressionMetrics.MeanSquaredError(y, estimator.Predict(x));
+        }
+    }
+}
